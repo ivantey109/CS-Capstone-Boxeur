7a7c3a96b310e81044e3c9c39e492f26299b360f
Evan Hopper-Moore <hopperme@oregonstate.edu>
2020-01-28 19:47:32 -0800

    Add styling for basic layout

---
+++ includes/editor.php
@@ -0,0 +1,37 @@
+<div class="row content">
+	<div class="col-sm-3" id="sidebar">
+        <div id='modes'>
+        <ul>
+           <li><a href='#'><span>Dimensions</span></a></li>
+           <!-- <li class='active has-sub'><a href='#'><span>Products</span></a>
+              <ul>
+                 <li class='has-sub'><a href='#'><span>Product 1</span></a>
+                    <ul>
+                       <li><a href='#'><span>Sub Product</span></a></li>
+                       <li class='last'><a href='#'><span>Sub Product</span></a></li>
+                    </ul>
+                 </li>
+                 <li class='has-sub'><a href='#'><span>Product 2</span></a>
+                    <ul>
+                       <li><a href='#'><span>Sub Product</span></a></li>
+                       <li class='last'><a href='#'><span>Sub Product</span></a></li>
+                    </ul>
+                 </li>
+              </ul>
+           </li> -->
+           <li><a href='#'><span>Edge Type</span></a></li>
+           <li class='last'><a href='#'><span>Holes</span></a></li>
+        </ul>
+        </div>
+
+        <hr>
+        <div id="sidebar-content"> <!-- this will change based on the mode selected above -->
+            <form>
+                <label for="width">Width</label>
+                <input type="text" name="width" value="">
+            </form>
+        </div>
+	</div>
+	<div class="col-sm-9" id="editor">
+	</div>
+</div>
